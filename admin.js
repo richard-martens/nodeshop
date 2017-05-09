8b2b9706b1164ea0172dc81f487e6881d7a45246
grischa17 <richard.martens@outlook.com>
2017-05-09 06:55:44 +0200

    bind the extracted middleware, set npm packages to specific version, switch connection string to localhost

--- admin.js
+++ admin.js
@@ -2,8 +2,10 @@
 var express = require('express');
 // Set up express
 var app = express();
+//Set up sessions
+var session = require('express-session')
 // Require mongostore session storage
-var mongoStore = require('connect-mongo')(express);
+var mongoStore = require('connect-mongo')(session);
 var passport = require('passport');
 var config = require('./shop/config.json');
 var info = require('./package.json');
@@ -12,55 +14,52 @@ var mongoose = require('mongoose');
 // User model and local strategy for passport
 var User = require('./schemas/user');
 var LocalStrategy = require('passport-local').Strategy;
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 
 console.log('NodeShop Admin Started!');
 
 // Passport authentication
-passport.use(new LocalStrategy({usernameField: 'email'},function(email, password, done) {User.authenticate(email, password, function(err, user) {return done(err, user)})}));
+passport.use(new LocalStrategy({ usernameField: 'email' }, function (email, password, done) { User.authenticate(email, password, function (err, user) { return done(err, user) }) }));
 
 // Session store
-passport.serializeUser(function(user, done) {done(null, user.id)});
-passport.deserializeUser(function(id, done) {User.findById(id, function (err, user) {done(err, user)})});
+passport.serializeUser(function (user, done) { done(null, user.id) });
+passport.deserializeUser(function (id, done) { User.findById(id, function (err, user) { done(err, user) }) });
 
 // Connect mongoose database
 mongoose.connect(config.connection);
 console.log('Connecting to database...');
 
 // Listen for mongoose connection
-mongoose.connection.on('open', function() {
+mongoose.connection.on('open', function () {
     console.log('Connected to database!');
 });
 
 // Configure Express
-app.configure(function(){
-    
-    // Set up jade
-    app.set('views', __dirname + '/admin/views');
-    app.set('view engine', 'jade');
-    
-    app.use(express.favicon());
-    app.use(express.cookieParser());
-    app.use(express.bodyParser());
+// Set up jade
+app.set('views', __dirname + '/admin/views');
+app.set('view engine', 'jade');
 
-    // Set up sessions
-    app.use(express.session({
-        // Set up MongoDB session storage
-        store: new mongoStore({url:config.connection}),
-        // Set session to expire after 21 days
-        cookie: { maxAge: new Date(Date.now() + 181440000)},
-        // Get session secret from config file
-        secret: config.cookie_secret
-        }));
-    
-    // Set up passport
-    app.use(passport.initialize());
-    app.use(passport.session());
-    
-    // Define public assets
-    app.use(express.static(__dirname + '/admin/public'));
-  
-});
-    
+app.use(cookieParser());
+app.use(bodyParser());
+
+// Set up sessions
+app.use(session({
+    // Set up MongoDB session storage
+    store: new mongoStore({ url: config.connection }),
+    // Set session to expire after 21 days
+    cookie: { maxAge: new Date(Date.now() + 181440000) },
+    // Get session secret from config file
+    secret: config.cookie_secret
+}));
+
+// Set up passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Define public assets
+app.use(express.static(__dirname + '/admin/public'));
+
 // Require router, passing passport for authenticating pages
 require('./admin/router')(app, passport);
 
@@ -70,6 +69,6 @@ app.listen(process.env.PORT);
 console.log('NodeShop v' + info.version + ' Admin Area listening on port ' + process.env.PORT);
 
 // Handle all uncaught errors
-process.on('uncaughtException', function(err) {
+process.on('uncaughtException', function (err) {
     console.log(err);
 });
--- shop.js
+++ shop.js
@@ -2,50 +2,50 @@
 var express = require('express');
 // Set up express
 var app = express();
+//Set up sessions
+var session = require('express-session')
 // Require mongostore session storage
-var mongoStore = require('connect-mongo')(express);
+var mongoStore = require('connect-mongo')(session);
 var passport = require('passport');
 // Require needed files
 var database = require('./shop/data');
 var config = require('./shop/config.json');
 var info = require('./package.json');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 
 console.log('NodeShop Started!');
 
 // Connect to database
 database.startup(config.connection);
 console.log('Connecting to database...');
-  
-// Configure Express
-app.configure(function(){
-    
-    // Set up jade
-    app.set('views', __dirname + '/shop/views');
-    app.set('view engine', 'jade');
-    
-    app.use(express.favicon());
-    app.use(express.cookieParser());
-    app.use(express.bodyParser());
 
-    // Set up sessions
-    app.use(express.session({
-        // Set up MongoDB session storage
-        store: new mongoStore({url:config.connection}),
-        // Set session to expire after 21 days
-        cookie: { maxAge: new Date(Date.now() + 181440000)},
-        // Get session secret from config file
-        secret: config.cookie_secret
-        }));
-    
-    // Set up passport
-    app.use(passport.initialize());
-    app.use(passport.session());
-    
-    // Define public assets
-    app.use(express.static(__dirname + '/shop/public'));
-  
-});
-    
+// Configure Express
+// Set up jade
+app.set('views', __dirname + '/shop/views');
+app.set('view engine', 'jade');
+
+app.use(cookieParser());
+app.use(bodyParser());
+
+// Set up sessions
+app.use(session({
+    // Set up MongoDB session storage
+    store: new mongoStore({ url: config.connection }),
+    // Set session to expire after 21 days
+    cookie: { maxAge: new Date(Date.now() + 181440000) },
+    // Get session secret from config file
+    secret: config.cookie_secret
+}));
+
+// Set up passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Define public assets
+app.use(express.static(__dirname + '/shop/public'));
+
+
 // Require router, passing passport for authenticating pages
 require('./shop/router')(app, passport);
 
@@ -55,6 +55,6 @@ app.listen(process.env.PORT);
 console.log('NodeShop v' + info.version + ' listening on port ' + process.env.PORT);
 
 // Handle all uncaught errors
-process.on('uncaughtException', function(err) {
+process.on('uncaughtException', function (err) {
     console.log(err);
 });
